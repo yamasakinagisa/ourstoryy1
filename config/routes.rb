cde44c90d7003fa35435874ff7fa8e1e590d0978
yamasakinagisa <60636075+yamasakinagisa@users.noreply.github.com>
2020-02-28 20:04:34 +0900

    serch

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, except: :index
-  
+  resources :posts do
+    resources :posts, except: :index
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
